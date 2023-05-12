4cb029489184297de9db37e82fca62fc09144686
Eduardo H Anacleto <typhonedu@gmail.com>
2023-05-12 02:15:05 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -13,6 +19,62 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/", () => "Hello World");
+
+app.MapGet("/frases", async () =>
+    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+);
+
+app.MapGet("/tarefas", async (AppDbContext db) => 
+    await db.Tarefas.ToListAsync());
+
+app.MapGet("/tarefas", async (AppDbContext db) => 
+    await db.Tarefas.Where(t=> t.IsConcluido).ToListAsync());
+
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa.Id);
+});
+
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if (tarefa is null) return Results.NotFound();
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluido = inputTarefa.IsConcluido;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+    return Results.NotFound();
+});
 
 app.Run();
 
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public Boolean IsConcluido { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
